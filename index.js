8612ff21315c7ccd6bb40e0ae6d09db1ecaa8679
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-10 11:45:51 +0300

    added required namespaces

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import builder from 'xmlbuilder';
 
+const xml = builder.create('SOAP-ENV:Envelope', {
+    version: '1.0',
+    encoding: 'UTF-8',
+})
+    .att('xmlns:SOAP-ENV', 'http://schemas.xmlsoap.org/soap/envelope/')
+    .att('xmlns:id', 'http://x-road.eu/xsd/identifiers')
+    .att('xmlns:xrd', 'http://x-road.eu/xsd/xroad.xsd')
+    .ele('SOAP-ENV:Header')
+    .end({ pretty: true});
+
+/*
 const xml = builder.create('root')
     .ele('xmlbuilder')
     .ele('repo', {'type': 'git'}, 'git://github.com/oozcitak/xmlbuilder-js.git')
     .end({ pretty: true});
+*/
 
 console.log(xml);
