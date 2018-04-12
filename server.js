313173e1907d0de9da8ff577b345c8df48ab38c6
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-12 13:14:06 +0300

    use adapter service for soap messages

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ import { parseString, processors } from 'xml2js';
 import morgan from 'morgan';
 
 import soapResponse from './index';
+import OpintoOikeusAdapterServer from './OpintoOikeusAdapterServer';
 
 require('body-parser-xml')(bodyParser);
 
 const app = express();
 const { stripPrefix } = processors;
+const adapterServer = new OpintoOikeusAdapterServer();
 
 app.use(morgan('combined')); // logging
 app.use(express.static('./docs')); // static content
@@ -25,7 +27,18 @@ app.post('/Endpoint', (req, res) => {
         console.dir(js, { depth: null });
     });
     res.set('Content-Type', 'text/xml');
-    res.send(soapResponse);
+
+    const clientXRoadInstance = 'FI-DEV';
+    const clientMemberClass = 'GOV';
+    const clientMemberCode = '2769790-1';
+    const clientSubsystemCode = 'koski';
+
+    const clientUserId = '123456789';
+    const clientRequestId = 'ID123456';
+
+    res.send(adapterServer.getOpintoOikeudetSoapResponse(clientXRoadInstance, clientMemberClass, clientMemberCode,
+        clientSubsystemCode, clientUserId, clientRequestId,
+    ));
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
