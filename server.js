eeb5324e6dd2515cb22c204049c30ac625bf8dc0
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-12 12:29:55 +0300

    refactoring

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-import soapResponse from './index';
-
 import express from 'express';
 import bodyParser from 'body-parser';
+import { parseString, processors } from 'xml2js';
+import morgan from 'morgan';
+
+import soapResponse from './index';
 
 require('body-parser-xml')(bodyParser);
 
-import morgan from 'morgan';
-
 const app = express();
+const { stripPrefix } = processors;
+
 app.use(morgan('combined')); // logging
 app.use(express.static('./docs')); // static content
 app.use(bodyParser.text({type: '*/*' }));
-
-var parseString = require('xml2js').parseString;
-var stripPrefix = require('xml2js').processors.stripPrefix;
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'application/xml');
@@ -21,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/Endpoint', (req, res) => {
-    parseString(req.body, { tagNameProcessors: [stripPrefix] }, function(err, js) {
+    parseString(req.body, { tagNameProcessors: [stripPrefix] }, (err, js) => {
         if (err) throw err;
         console.dir(js, { depth: null });
     });
