b5b7553916268b9f386a6fa6b8dd6a3c43ea119e
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-14 16:17:45 +0300

    code style

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 const babel = require('gulp-babel');
-var gutil = require('gulp-util');
-var del = require('del');
-var rename = require('gulp-rename');
-var install = require('gulp-install');
-var zip = require('gulp-zip');
-var fs = require('fs');
-var runSequence = require('run-sequence');
+const gutil = require('gulp-util');
+const del = require('del');
+const rename = require('gulp-rename');
+const install = require('gulp-install');
+const zip = require('gulp-zip');
+const fs = require('fs');
+const runSequence = require('run-sequence');
 
 const paths = {
     srcJS: 'src/**/*.js',
@@ -17,7 +17,7 @@ const paths = {
 };
 
 // First we need to clean out the dist folder and remove the compiled zip file.
-gulp.task('clean', function(cb) {
+gulp.task('clean', (cb) => {
     return del([
         'dist',
         'dist.zip',
@@ -31,22 +31,21 @@ gulp.task('js', () =>
 );
 
 // Next copy over environment variables managed outside of source control.
-gulp.task('env', function() {
+gulp.task('env', () => {
     gulp.src('./config.env.development')
         .pipe(rename('.env'))
         .pipe(gulp.dest('./dist'));
 });
 
-
 // Here we want to install npm packages to dist, ignoring devDependencies.
-gulp.task('npm', function(callback) {
+gulp.task('npm', (callback) => {
     gulp.src('./package.json')
         .pipe(gulp.dest('./dist/'))
         .pipe(install({ production: true }, callback));
 });
 
 // Now the dist directory is ready to go. Zip it.
-gulp.task('zip', function() {
+gulp.task('zip', () => {
     gulp.src(['dist/**/*', '!dist/package.json', 'dist/.*', 'dist/node_modules'])
         .pipe(zip('dist.zip'))
         .pipe(gulp.dest('./'));
@@ -59,42 +58,42 @@ gulp.task('zip', function() {
 // the case if you have installed and configured the AWS CLI.
 //
 // See http://aws.amazon.com/sdk-for-node-js/
-gulp.task('upload', function() {
+gulp.task('upload', () => {
 
     // TODO: This should probably pull from package.json
     AWS.config.region = 'us-east-1';
-    var lambda = new AWS.Lambda();
-    var functionName = 'video-events';
+    const lambda = new AWS.Lambda();
+    const functionName = 'video-events';
 
-    lambda.getFunction({FunctionName: functionName}, function(err, data) {
+    lambda.getFunction({FunctionName: functionName}, (err, data) => {
         if (err) {
             if (err.statusCode === 404) {
-                var warning = 'Unable to find lambda function ' + deploy_function + '. '
-                warning += 'Verify the lambda function name and AWS region are correct.'
+                const warning = 'Unable to find lambda function ' + deploy_function + '. ';
+                warning += 'Verify the lambda function name and AWS region are correct.';
                 gutil.log(warning);
             } else {
-                var warning = 'AWS API request failed. '
-                warning += 'Check your AWS credentials and permissions.'
+                const warning = 'AWS API request failed. ';
+                warning += 'Check your AWS credentials and permissions.';
                 gutil.log(warning);
             }
         }
 
         // This is a bit silly, simply because these five parameters are required.
-        var current = data.Configuration;
-        var params = {
+        const current = data.Configuration;
+        const params = {
             FunctionName: functionName,
             Handler: current.Handler,
             Mode: current.Mode,
             Role: current.Role,
-            Runtime: current.Runtime
+            Runtime: current.Runtime,
         };
 
-        fs.readFile('./dist.zip', function(err, data) {
+        fs.readFile('./dist.zip', (err, data) => {
             params['FunctionZip'] = data;
-            lambda.uploadFunction(params, function(err, data) {
+            lambda.uploadFunction(params, (err, data) => {
                 if (err) {
-                    var warning = 'Package upload failed. '
-                    warning += 'Check your iam:PassRole permissions.'
+                    const warning = 'Package upload failed. ';
+                    warning += 'Check your iam:PassRole permissions.';
                     gutil.log(warning);
                 }
             });
@@ -103,7 +102,7 @@ gulp.task('upload', function() {
 });
 
 // The key to deploying as a single command is to manage the sequence of events.
-gulp.task('default', function(callback) {
+gulp.task('default', (callback) => {
     return runSequence(
         ['clean'],
         ['js', 'npm', 'env'],
