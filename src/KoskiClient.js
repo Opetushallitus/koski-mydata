35a3c1384b4e7def32e8387791a54463f455b462
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-19 14:30:37 +0300

    await for api call responses

--- src/KoskiClient.js
+++ src/KoskiClient.js
@@ -4,7 +4,7 @@ import deepOmit from 'omit-deep-lodash';
 class KoskiClient {
     constructor(username, password) {
         this.instance = axios.create({
-            baseURL: 'https://dev.koski.opintopolku.fi/koski/api/',
+            baseURL: 'https://dev.koski.opintopolku.fi/koski/api/', // TODO: Read URL from environment variables
             timeout: 5000,
             auth: {
                 username,
@@ -13,32 +13,31 @@ class KoskiClient {
         });
     }
 
-    async getUserOid(hetu) {
-        return new Promise((resolve, reject) => {
-            this.instance.get(`henkilo/search?query=${hetu}`)
-                .then((response) => {
-                    const students = response.data['henkilöt'];
-                    if (students.length < 1) reject(new Error('no users found'));
+    getUserOid(hetu) {
+        return new Promise( async(resolve, reject) => {
+            try {
+                const response = await this.instance.get(`henkilo/search?query=${hetu}`);
+                const students = response.data['henkilöt'];
+                if (students.length < 1) reject(new Error('no users found'));
 
-                    resolve(students[0].oid);
-                })
-                .catch((error) => {
-                    // error contains hetu and credentials, lets not log them
-                    reject(new Error(`Henkilo search failed with status: ${error.response.status}`));
-                });
+                resolve(students[0].oid);
+            } catch (err) {
+                // error contains credentials, url contains hetu, lets not log them
+                reject(new Error(`Henkilo search failed with status: ${err.response.status}`));
+            }
         });
     }
 
-    async getOpintoOikeudet(oid) {
-        return new Promise((resolve, reject) => {
-            this.instance.get(`oppija/${oid}`)
-                .then((response) => {
-                    const { opiskeluoikeudet } = response.data;
-                    resolve(deepOmit(opiskeluoikeudet, 'suoritukset')); // remove 'suoritukset' from response
-                })
-                .catch((error) => {
-                    reject(error);
-                });
+    getOpintoOikeudet(oid) {
+        return new Promise( async(resolve, reject) => {
+            try {
+                const response = await this.instance.get(`oppija/${oid}`);
+                const { opiskeluoikeudet } = response.data;
+                resolve(deepOmit(opiskeluoikeudet, 'suoritukset')); // remove 'suoritukset' from response
+            } catch (err) {
+                // error contains credentials, url contains hetu, lets not log them
+                reject(new Error(`Suoritukset search failed with status: ${err.response.status}`));
+            }
         });
     }
 }
