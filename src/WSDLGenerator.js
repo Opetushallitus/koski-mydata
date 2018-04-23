08e2a64340b8445a5e1ef8774a0e1821be03687b
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-23 20:56:25 +0300

    implemented rest of the wsdl

--- src/WSDLGenerator.js
+++ src/WSDLGenerator.js
@@ -40,7 +40,6 @@ class WSDLGenerator {
 
             .up().up().up().up().up().up()
 
-
             .ele('xsd:element')
             .att('name', 'opintoOikeudetServiceResponse')
 
@@ -66,7 +65,6 @@ class WSDLGenerator {
 
             .up()
 
-
             .ele('wsdl:message').att('name', 'opintoOikeudetService')
             .ele('wsdl:part').att('name', 'body').att('element', 'tns:opintoOikeudetService').up()
 
@@ -74,6 +72,43 @@ class WSDLGenerator {
 
             .ele('wsdl:message').att('name', 'opintoOikeudetServiceResponse')
             .ele('wsdl:part').att('name', 'body').att('element', 'tns:opintoOikeudetServiceResponse').up()
+
+            .up()
+
+            .ele('wsdl:portType').att('name', 'opintoOikeudetServicePortType')
+            .ele('wsdl:operation').att('name', 'opintoOikeudetService')
+            .ele('wsdl:input').att('message', 'tns:opintoOikeudetService')
+            .ele('wsdl:output').att('message', 'tns:opintoOikeudetServiceResponse')
+
+            .up().up().up().up()
+
+            .ele('wsdl:binding').att('name', 'opintoOikeudetServiceBinding').att('type', 'tns:opintoOikeudetServicePortType')
+            .ele('soap:binding').att('style', 'document').att('transport', 'http://schemas.xmlsoap.org/soap/http')
+            .ele('wsdl:operation').att('name', 'opintoOikeudetService')
+            .ele('soap:operation').att('soapAction', '').att('style', 'document').up()
+            .ele('xrd:version').text('v1').up()
+            .ele('wsdl:input')
+            .ele('soap:body').att('parts', 'body').att('use', 'literal')
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'client').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'service').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'userId').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'id').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'issue').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'protocolVersion').att('use', 'literal').up()
+            .up().up()
+            .ele('wsdl:output')
+            .ele('soap:body').att('parts', 'body').att('use', 'literal')
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'client').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'service').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'userId').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'id').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'issue').att('use', 'literal').up()
+            .ele('soap:header').att('message', 'tns:requestheader').att('part', 'protocolVersion').att('use', 'literal').up()
+            .up().up().up().up().up()
+
+            .ele('wsdl:service').att('name', 'opintoOikeudetService')
+            .ele('wsdl:port').att('binding', 'tns:opintoOikeudetServiceBinding').att('name', 'opintoOikeudetServicePort')
+            .ele('soap:address').att('location', 'https://e5tcdum0og.execute-api.eu-west-1.amazonaws.com/dev') // TODO API URL
 
             .end({ pretty: true });
     }
