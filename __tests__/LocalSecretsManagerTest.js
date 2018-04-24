8862ba2afd49f8fecfcd37ca56c68e1e63b0da13
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-24 21:12:15 +0300

    tests for local secrets manager

---
+++ __tests__/LocalSecretsManagerTest.js
@@ -0,0 +1,40 @@
+import LocalSecretsManager from '../src/LocalSecretsManager';
+
+describe('LocalSecretsManager', () => {
+    beforeEach(() => {
+        jasmine.addMatchers(require('jasmine-node-promise-matchers'));
+    });
+
+    const expectedCredentials = { // This is what we expect to return after AWS response has been parsed
+        username: 'username',
+        password: 'password',
+    };
+
+    it('Should return credentials from process.env', async(done) => {
+        process.env = {
+            KOSKI_USER: expectedCredentials.username,
+            KOSKI_PASSWORD: expectedCredentials.password,
+        };
+        const receivedCredentials = await new LocalSecretsManager().getKoskiCredentials();
+        expect(expectedCredentials).toEqual(receivedCredentials);
+        done();
+    });
+
+    process.env.KOSKI_USER = undefined;
+
+    it('Should fail if no username was given', async(done) => {
+        process.env = {
+            KOSKI_USER: undefined,
+            KOSKI_PASSWORD: expectedCredentials.password,
+        };
+        expect(new LocalSecretsManager().getKoskiCredentials()).toRejectWith(new Error('username not defined'), done);
+    });
+
+    it('Should fail if no password was given', async(done) => {
+        process.env = {
+            KOSKI_USER: expectedCredentials.username,
+            KOSKI_PASSWORD: null,
+        };
+        expect(new LocalSecretsManager().getKoskiCredentials()).toRejectWith(new Error('password not defined'), done);
+    });
+});
