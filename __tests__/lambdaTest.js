cd6a154dfd2a7af0b56b94555b0066bd4e6f0bf7
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-25 16:37:57 +0300

    test that only WSDL GET requests are served

--- __tests__/lambdaTest.js
+++ __tests__/lambdaTest.js
@@ -15,10 +15,36 @@ describe('Lambda', () => {
             httpMethod: 'GET',
             queryStringParameters: { wsdl: true },
         };
+
         WSDLGenerator.createOpintoOikeusWSDL = () => WSDL;
+        spyOn(WSDLGenerator, 'createOpintoOikeusWSDL').and.callThrough();
+
         opintoOikeusHandler(event, null, (error, response) => {
             expect(error).toBeNull();
             expect(response).toEqual(expectedResponse);
+            expect(WSDLGenerator.createOpintoOikeusWSDL).toHaveBeenCalled();
+            done();
+        });
+    });
+
+    it('Will only serve GET WSDL requests', async(done) => {
+        const expectedResponse = {
+            statusCode: 200,
+            headers: {
+                'content-type': 'application/wsdl+xml',
+            },
+        };
+        const event = {
+            httpMethod: 'GET',
+            queryStringParameters: { },
+        };
+
+        spyOn(WSDLGenerator, 'createOpintoOikeusWSDL').and.callThrough();
+
+        opintoOikeusHandler(event, null, (error, response) => {
+            expect(error).toBeNull();
+            expect(response.statusCode).toEqual(500);
+            expect(WSDLGenerator.createOpintoOikeusWSDL).not.toHaveBeenCalled();
             done();
         });
     });
