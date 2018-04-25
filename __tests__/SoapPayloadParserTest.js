b06279585f5b1e92119a1953827d4c30e4cee080
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-04-25 14:13:58 +0300

    test for incoming soap payload parser

---
+++ __tests__/SoapPayloadParserTest.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import SoapPayloadParser from '../src/SoapPayloadParser';
+
+describe('SoapPayloadParser', () => {
+    beforeEach(() => {
+        jasmine.addMatchers(require('jasmine-node-promise-matchers'));
+    });
+
+    const parser = new SoapPayloadParser();
+
+    it('Should be able to parse reference payload', (done) => {
+        fs.readFile('./examples/opintooikeudet-payload.xml', { encoding: 'UTF-8'}, (err, xml) => {
+            if (err) throw err;
+
+            const parseResult = parser.parsePayload(xml);
+
+            const expectedResult = {
+                clientXRoadInstance: 'FI-DEV',
+                clientMemberClass: 'GOV',
+                clientMemberCode: '2769790-1',
+                clientSubsystemCode: 'koski',
+                clientUserId: '123456789',
+                clientRequestId: 'ID123456',
+                clientType: 'SUBSYSTEM',
+                hetu: '210947-613P',
+            };
+
+            expect(parseResult).toEqual(expectedResult);
+            done();
+        });
+    });
+});
