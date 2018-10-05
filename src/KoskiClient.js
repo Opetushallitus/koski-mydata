77bfb93abc09621176facaf12af11e9cba3c75e6
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2018-10-05 13:39:26 +0300

    remove koulutusvienti from response

--- src/KoskiClient.js
+++ src/KoskiClient.js
@@ -14,6 +14,7 @@ const blacklistedOpiskeluOikeudetFields = [
     'oikeusMaksuttomaanAsuntolapaikkaan',
     'majoitus',
     'henkilöstökoulutus',
+    'koulutusvienti',
 ];
 
 const blacklistedStudentFields = [
