4eb818883e206296fa944e6580c663ba4b979740
Juuso Strömberg <juuso.stromberg@gmail.com>
2018-10-16 14:05:11 +0300

    Upload to utility account

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 ENVIRONMENT=${1:-"dev"}
 
-aws s3 cp "s3://oph-koski-omadata-artifacts-$ENVIRONMENT/sovitinpalvelu/latest.zip" "s3://oph-koski-omadata-artifacts-$ENVIRONMENT/sovitinpalvelu/$(date "+%Y-%m-%d-%H-%M-%S").zip"
-aws s3 cp dist.zip "s3://oph-koski-omadata-artifacts-$ENVIRONMENT/sovitinpalvelu/latest.zip"
-aws lambda update-function-code --function-name get-opinto-oikeudet-$ENVIRONMENT --s3-bucket oph-koski-omadata-artifacts-$ENVIRONMENT --s3-key sovitinpalvelu/latest.zip
+aws s3 cp "s3://oph-koski-omadata-sovitinpalvelu/latest.zip" "s3://oph-koski-omadata-sovitinpalvelu/$(date "+%Y-%m-%d-%H-%M-%S").zip"
+aws s3 cp dist.zip "s3://oph-koski-omadata-sovitinpalvelu/latest.zip"
+aws lambda update-function-code --function-name get-opinto-oikeudet-$ENVIRONMENT --s3-bucket oph-koski-omadata-sovitinpalvelu --s3-key latest.zip --profile oph-koski-$ENVIRONMENT
