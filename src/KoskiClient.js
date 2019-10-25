a4c361e8a8e957488d480d72ecb2151b8c2e2903
Antero Fagerstedt <antero.fagerstedt@reaktor.com>
2019-10-25 15:16:27 +0300

    TOR-798: Add expiration date of user's permission to response

--- src/KoskiClient.js
+++ src/KoskiClient.js
@@ -105,7 +105,7 @@ class KoskiClient {
 
                 const response = await this._executeOppijaDataRequest(hetu, clientMemberCode);
 
-                const { henkilö, opiskeluoikeudet } = response.data;
+                const { henkilö, opiskeluoikeudet, suostumuksenPaattymispaiva } = response.data;
 
                 if (typeof opiskeluoikeudet === 'undefined' || opiskeluoikeudet === null) reject(new Error('No opiskeluoikeudet found'));
 
@@ -135,6 +135,7 @@ class KoskiClient {
                 resolve({
                     henkilö: deepOmit(henkilö, ...blacklistedStudentFields),
                     opiskeluoikeudet: filteredOpiskeluoikeudet,
+                    suostumuksenPaattymispaiva,
                 });
             } catch (err) {
                 // error contains credentials, url contains hetu, lets not log them
